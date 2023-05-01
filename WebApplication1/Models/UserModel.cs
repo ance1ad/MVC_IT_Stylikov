1db04ca1308e07965191349bd569e672323bca9e
ance1ad <109547449+ance1ad@users.noreply.github.com>
2023-05-01 16:56:43 +0300

    start stage

---
+++ WebApplication1/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    // модель пользователя
+    public class UserModel
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ WebApplication1/Utils/UserHelper.cs
@@ -0,0 +1,26 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Utils
+{
+    // как я понял тут будут хранится данные о пользователе
+    public class UserHelper
+    {
+        public static IEnumerable<UserModel> GetUsers() =>
+            new List<UserModel>()
+            {
+                new UserModel()
+                {   Login = "Viktor123",
+                    Password= "pw12",
+                    Name = "Viktor",
+                    Age = 23
+                },
+                new UserModel()
+                {   Login = "Viktor123",
+                    Password= "pw12",
+                    Name = "Alex",
+                    Age = 21
+                },
+            };
+
+    }
+}
